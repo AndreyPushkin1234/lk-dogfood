ef9107dea4e04cb72562b44def4db7879569c36b
Andrey <qazwsxedc20108@mail.ru>
2023-07-03 05:14:43 +0300

    dogfood

---
+++ lk-dogfood/src/components/Banner/index.jsx
@@ -0,0 +1,8 @@
+import "./style.css"
+
+const Banner = () => <div>
+    <h4 className="title">Самый вкусный, корм для собак <br />лапки оближешь</h4>
+    <p className="description">Качество, вкус, здоровье<br/>с доставкой по Москве и регионам</p>
+</div>
+
+export default Banner
